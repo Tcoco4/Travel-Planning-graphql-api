93a23e27627f38fcbe36daf21a51d7300d352a1e
Tcoco4 <taremautsa@yahoo.com>
2025-08-11 21:51:42 +0200

    queries & resolvers defined

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -2,10 +2,45 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    city: City
+    suggestCities(countryName: String!): [City!]!
+    weatherForCity(id: ID!): WeatherForecast!
+    activitiesRankedInCity(id: ID!): [ActivityScore!]!
   }
+
   type City {
+    id: ID!
     name: String!
+    country: String
+    lat: Float!
+    lon: Float!
+    timezone: String
+  }
+
+  type WeatherForecast {
+    latitude: Float!
+    longitude: Float!
+    timezone: String!
+    dayForcast: DayForecast
+  }
+
+  type DayForecast {
+    time: [String!]!
+    temperature_min: [Float!]!
+    temperature_max: [Float!]!
+    precipitation: [Float!]!
+    weatherCode: [Int!]!
+  }
+
+  type ActivityScore {
+    activity: Activity!
+    score: Int!
+    reasons: [String!]!
+  }
+  enum Activity {
+    SKIING
+    SURFING
+    INDOOR_SIGHTSEEING
+    OUTDOOR_SIGHTSEEING
   }
 `;
 module.exports = { typeDefs };
