ea6e1583f83fb7aebcf180f86bc749e267b4b26f
Tcoco4 <taremautsa@yahoo.com>
2025-08-12 12:21:09 +0200

    tidy up schema

--- src/schema/typeDefs.js
+++ src/schema/typeDefs.js
@@ -1,89 +1,86 @@
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
+  #Query Definiton
   type Query {
     suggestCities(countryName: String): [City!]!
     weatherForCity(input: WeatherForecastInput): WeatherForecast!
     activitiesRankedInCity(id: ID!): [ActivityScore!]!
   }
 
+  #Scalar Definition
+  scalar DateTime
+
+  #Type Definition
   type City {
     id: ID!
     name: String!
     country: String
-    lat: Float!
-    lon: Float!
+    latitude: Float!
+    longitude: Float!
     timezone: String
   }
 
   type WeatherForecast {
-    latitude: Float!
-    longitude: Float!
+    latitude: Float
+    longitude: Float
+    generationtime_ms: Float
+    utc_offset_seconds: Int
     timezone: String
-    forecast_days: DayForecast
+    timezone_abbreviation: String
+    elevation: Float
+    daily_units: DailyWeatherUnits!
+    daily: DailyWeather!
   }
-  type DayForecast {
-    time: [String!]!
-    temperature_min: [Float!]!
-    temperature_max: [Float!]!
-    precipitation: [Float!]!
-    weatherCode: [Int!]!
+  type DailyWeatherUnits {
+    time: String
+    temperature_2m_min: String
+    temperature_2m_max: String
+    windspeed_10m_min: String
+    windspeed_10m_max: String
+    sunrise: DateTime
+    sunset: DateTime
+    precipitation_sum: String
+    weathercode: String
   }
-  # type WeatherForecast {
-  #     latitude: Float
-  #     longitude: Float,
-  #     generationtime_ms:Float,
-  #     utc_offset_seconds: Int,
-  #     timezone: String,
-  #     timezone_abbreviation: String,
-  #     elevation: Float,
-  #     daily_units: {
-  #         time: String,
-  #         temperature_2m_min: String,
-  #         temperature_2m_max: String,
-  #         windspeed_10m_min: String,
-  #         windspeed_10m_max: String,
-  #         sunrise: String,
-  #         sunset: String,
-  #         precipitation_sum: String,
-  #         weathercode: String
-  #     },
-  #     daily: {
-  #         time: [DateString],
-  #         temperature_2m_min: [Float],
-  #         temperature_2m_max: [Float],
-  #         windspeed_10m_min: [Float],
-  #         windspeed_10m_max: [Float],
-  #         sunrise: [DateTime],
-  #         sunset: [DateTime],
-  #         precipitation_sum: [Float]
-  #         weathercode: [Float]
-  #     }
-  # }
-
-  input WeatherForecastInput {
-    latitude: Float!
-    longitude: Float!
-    daily: DailyWeatherInput
-    timezone: String = auto
-    forecast_days: Int
-  }
-
-  input DailyWeatherInput {
-    temperature_2m_min: String = temperature_2m_min
-    temperature_2m_max: String = temperature_2m_max
-    windspeed_10m_min: String = windspeed_10m_min
-    windspeed_10m_max: String = windspeed_10m_max
-    sunrise: String = sunrise
-    sunset: String = sunset
-    precipitation_sum: String = precipitation_sum
-    weathercode: String = weathercode
+  type DailyWeather {
+    time: [String]
+    temperature_2m_min: [Float]
+    temperature_2m_max: [Float]
+    windspeed_10m_min: [Float]
+    windspeed_10m_max: [Float]
+    sunrise: [String]
+    sunset: [String]
+    precipitation_sum: [Float]
+    weathercode: [Float]
   }
   type ActivityScore {
     activity: Activity!
     score: Int!
     reasons: [String!]!
   }
+
+  #Input Definitions
+  input WeatherForecastInput {
+    latitude: Float!
+    longitude: Float!
+    daily: DailyWeatherInput
+    timezone: String = auto
+    forecast_days: Int = 3
+  }
+
+  input DailyWeatherInput {
+    temperature_2m_min: String = "temperature_2m_min"
+    temperature_2m_max: String = "temperature_2m_max"
+    windspeed_10m_min: String = "windspeed_10m_min"
+    windspeed_10m_max: String = "windspeed_10m_max"
+    sunrise: String = "sunrise"
+    sunset: String = "sunset"
+    precipitation_sum: String = "precipitation_sum"
+    weathercode: String = "weathercode"
+  }
+
+  #Enum definitions
   enum Activity {
     SKIING
     SURFING
