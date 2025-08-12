2cd1f0eddcd31666e2f4ead62a50b14693d00f05
Tcoco4 <taremautsa@yahoo.com>
2025-08-12 14:24:59 +0200

    openMeteo client added

--- src/client/openMeteoClient.js
+++ src/client/openMeteoClient.js
@@ -1,46 +1,92 @@
 export default class OpenMeteoClient {
   geocodeURL = "https://geocoding-api.open-meteo.com/v1/search";
   weatherUrl = "https://api.open-meteo.com/v1/forecast";
-  async geocode(q, limit = 8, language = "en") {
+
+  async getGeoLocationData(city, limit = 1, language = "en", format = "json") {
+    //Modify to accept country
+    let response;
     const url = `${this.geocodeUrl}?name=${encodeURIComponent(
-      q
-    )}&count=${limit}&language=${language}`;
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("Geocoding failed");
-    return res.json(); // returns results array
+      city
+    )}&count=${limit}&language=${language}$format=${format}`;
+
+    await fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.results && data.results.length > 0) {
+          const { latitude, longitude, name } = data.results[0];
+
+          return (response = {
+            success: true,
+            data: { latitude: latitude, longitude: longitude, name: name },
+            message: "location found",
+          });
+        } else
+          return (response = {
+            success: false,
+            data: null,
+            message: "location not found",
+          });
+      })
+      .catch(
+        (e) =>
+          (response = {
+            success: false,
+            message: `Error ${e}` || "Error Occured",
+          })
+      );
   }
 
-  async forecast(
+  async getWeatherForecastData(
     lat,
     lon,
-    timezone = "auto",
-    hourly = [
-      "temperature_2m",
-      "precipitation",
-      "windspeed_10m",
-      "weathercode",
-    ],
     daily = [
       "temperature_2m_min",
       "temperature_2m_max",
       "precipitation_sum",
       "weathercode",
     ],
-    start,
-    end
+    timezone = "auto",
+    forecast_days = 4
   ) {
     const params = new URLSearchParams({
       latitude: String(lat),
       longitude: String(lon),
-      timezone,
-      hourly: hourly.join(","),
       daily: daily.join(","),
+      timezone,
+      forecast_days,
     });
-    if (start) params.set("start_date", start);
-    if (end) params.set("end_date", end);
 
     const url = `${this.weatherUrl}?${params.toString()}`;
-    const res = await fetch(url);
+
+    await fetch(url)
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        if (!data)
+          return (response = {
+            success: false,
+            message: " Weather data not found",
+            data: null,
+          });
+        else {
+          const { daily } = data;
+
+          return (response = {
+            success: true,
+            message: "Weather data found",
+            data: transform(daily),
+          });
+        }
+      })
+      .catch(
+        (e) =>
+          (response = {
+            success: false,
+            message: `Error ${e} ` || "Error Occured",
+          })
+      );
+
     if (!res.ok) throw new Error("Weather API failed");
     return res.json();
   }
