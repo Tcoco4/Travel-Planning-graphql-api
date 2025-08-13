3708cb46552b8c08da3644bcf5c34755b1e1b6ac
Tcoco4 <taremautsa@yahoo.com>
2025-08-13 16:08:31 +0200

    all activities ranked

--- src/services/activityRankingService.js
+++ src/services/activityRankingService.js
@@ -30,19 +30,79 @@ export class ActivityRankingService {
     });
     return scoring;
   }
-  scoreSurfing() {}
+  scoreSurfing(weatherData) {
+    const scoring = weatherData.map((day, index) => {
+      const {
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum,
+      } = day;
+      const result = isSurfDay(
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum
+      );
+      return { date: day.date, reason: result.reason, score: result.score };
+    });
+    return scoring;
+  }
 
-  scoreOutdoor() {}
+  scoreOutdoor(weatherData) {
+    const scoring = weatherData.map((day, index) => {
+      const {
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum,
+      } = day;
+      const result = isOutdoorDay(
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum
+      );
+      return { date: day.date, reason: result.reason, score: result.score };
+    });
+    return scoring;
+  }
 
-  scoreIndoor() {}
+  scoreIndoor(weatherData) {
+    const scoring = weatherData.map((day, index) => {
+      const {
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum,
+      } = day;
+      const result = isIndoorDay(
+        temperature_2m_min,
+        temperature_2m_max,
+        windspeed_10m_min,
+        windspeed_10m_max,
+        precipitation_sum
+      );
+      return { date: day.date, reason: result.reason, score: result.score };
+    });
+    return scoring;
+  }
   async rankActivities(input, weatherData) {
     let results = [];
     const skiing = this.scoreSkiing(weatherData);
-    // const surfing = this.scoreSurfing(weatherData);
-    // const outdoorSightSee = this.scoreOutdoor(weatherData);
-    // const indoorSightSee = this.scoreIndoor(weatherData);
+    const surfing = this.scoreSurfing(weatherData);
+    const outdoor = this.scoreOutdoor(weatherData);
+    const indoor = this.scoreIndoor(weatherData);
 
     results.push({ activity: "SKIING", details: skiing });
+    results.push({ activity: "SURFING", details: surfing });
+    results.push({ activity: "OUTDOOR_SIGHTSEEING", details: outdoor });
+    results.push({ activity: "INDOOR_SIGHTSEEING", details: indoor });
 
     return results;
   }
